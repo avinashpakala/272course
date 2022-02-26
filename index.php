666911ad274f233c1fffd852156e15713f6e99a8
Avinash <avinash.pakala93@gmail.com>
2022-02-25 18:04:00 -0800

    modified index file

--- index.php
+++ index.php
@@ -1,26 +1,4 @@
 <?php
-  echo "This is my firt test php for CMPE 272 class..!"
+  echo "This is php setup test for 272 course"
 ?>
 
-<!DOCTYPE html>
-<html>
-<body>
-
-<canvas id="myCanvas" width="400" height="300"
-style="border:1px solid #c3c3c3;">
-Your browser does not support the canvas element.
-</canvas>
-
-<script>
-var canvas = document.getElementById("myCanvas");
-var ctx = canvas.getContext("2d");
-ctx.beginPath();
-ctx.arc(95, 50, 40, 0, 2 * Math.PI);
-ctx.stroke();
-
-ctx.fillRect(0,110,80,75);
-ctx.fillRect(80,75,150,155)
-</script>
-
-</body>
-</html>
