d4f1d74e44ad863e3199f899f714e97ca2be2a49
Avinash <avinash.pakala93@gmail.com>
2022-04-22 17:00:19 -0700

    update

--- pages/user/curl.php
+++ pages/user/curl.php
@@ -1,8 +1,10 @@
 <?php
 $curl_handle = curl_init();
                     curl_setopt($curl_handle, CURLOPT_URL, "https://qaraar.org/search-user.php");
+                    curl_setopt($curl_handle, CURLOPT_POSTFIELDS, 'userIn=admin&passIn=password');
                     curl_setopt($curl_handle, CURLOPT_HEADER, 0);
                     curl_setopt($curl_handle,CURLOPT_RETURNTRANSFER,true);
+
                     $contents = curl_exec($curl_handle);
                    
                     curl_close($curl_handle);
